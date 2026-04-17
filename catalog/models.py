2d55bdcc04359a6fb84c174122a479e6dcf84a24
Vlad <vlad@exampl.com>
2026-04-17 15:35:54 +0300

    Created new project library

---
+++ catalog/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser 
+from django.conf import settings
+
+class LiteraryFormat(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.format
+    
+    class Meta:
+        ordering = ("name",)
+
+
+class Author(AbstractUser):
+    class Meta:
+        ordering = ("username",)
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    format = models.ForeignKey(
+        LiteraryFormat,
+        on_delete=models.CASCADE,
+        related_name="books"
+    )
+    authors = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="books"
+    )
+
+    class Meta:
+        ordering = ("title",)
