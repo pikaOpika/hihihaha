f923019b854f03671fb6020774d79becf9dfd5ce
Vlad <vlad.grinkiv@stud.onu.edu.ua>
2026-04-18 17:10:31 +0300

    working with admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from catalog.models import LiteraryFormat, Book, Author
 
 # Register your models here.
+# admin.site.register(LiteraryFormat)
+# admin.site.register(Book)
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 1
+
+@admin.register(LiteraryFormat)
+class LiteraryFormatAdmin(admin.ModelAdmin):
+    inlines = [BookInline]
+
+# class AuthorInline(admin.TabularInline):
+#     model = Author
+#     extra = 1
+
+
+# @admin.register(Book)
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ["title", "price", "format"]
+#     search_fields = ["title"]
+#     list_filter = ["title"]
+#     inlines = [AuthorInline]
+
+admin.site.register(Author, UserAdmin)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["title", "price", "format"]
+    ordering = ["title"]
+    search_fields = ["title"]
+    list_filter = ["format"]
+    actions = ["change_format"]
+
+    def change_format(self, request, queryset):
+        novel = LiteraryFormat.objects.get(name="novel")
+        queryset.update(format=novel)
+
+    change_format.short_description = "Change format"
+    # fieldsets = [
+    #     ("main information", {
+    #         "fields": ["title", "format"]
+    #     })
+    # ]
--- catalog/models.py
+++ catalog/models.py
@@ -6,7 +6,7 @@ class LiteraryFormat(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.format
+        return self.name
     
     class Meta:
         ordering = ("name",)
