506dbe870697ed9acaf5dca3302edb0f9b0937f8
Vlad <vlad@exampl.com>
2026-04-21 13:45:04 +0300

    added static files it wasnt so hard as i thought lucky me

--- library/settings.py
+++ library/settings.py
@@ -116,5 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 AUTH_USER_MODEL = "catalog.Author"
